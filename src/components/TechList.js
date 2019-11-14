44f663e78bcfad7cf06e193be7f9f1c178f749f7
scard69 <vit.scard@gmail.com>
2019-11-14 00:20:18 -0300

    iniciando com react

---
+++ src/components/TechList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+class TechList extends Component{
+  state = {
+    techs: [
+      'Node.js',
+      'React JS',
+      'React Native'
+
+    ]
+  };
+
+  render() {
+    return (
+      <ul>
+        <li>Node.js</li>
+        <li>React JS</li>
+        <li>React Native</li>
+      </ul>
+    );
+
+  }
+}
+
+export default TechList;
